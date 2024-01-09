cbcfe4a1da2f4c7e87ab8fc95f1a166fd845a3e6
fs-99 <135445356+fs-99@users.noreply.github.com>
2024-01-09 18:52:50 +0100

    add unhygienic serial print macro

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,44 @@
 
 use core::panic::PanicInfo;
 
+use uart_16550::SerialPort;
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // SAFETY: the base address should be right
+    // TODO: all port addresses could be bound only once, preventing access by someone else
+    let mut serial_port = unsafe {
+        SerialPort::new(0x3F8)
+    }; 
+    serial_port.init();
+    // TODO: I left out Mutex here for now
+    let mut printer = DebugPrinter::new(serial_port);
+    
+    /// Prints to the host through the serial interface.
+    #[macro_export]
+    macro_rules! serial_print {
+        ($($arg:tt)*) => {
+            printer._print(format_args!($($arg)*));
+        };
+    }
+
+    serial_print!("test");
+
     loop {}
+}
+
+struct DebugPrinter {
+    serial: SerialPort,
+}
+
+impl DebugPrinter {
+    pub fn new(serial: SerialPort) -> Self {
+        Self { serial }
+    }
+    pub fn _print(&mut self, args: ::core::fmt::Arguments) {
+        use core::fmt::Write;
+        self.serial.write_fmt(args).expect("Printing to serial failed");
+    }
 }
 
 #[panic_handler]
